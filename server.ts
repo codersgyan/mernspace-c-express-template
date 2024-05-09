15ba3576257bc65483f5e0462b17c0b6444b05ad
Rakesh K <codersgyan@gmail.com>
2024-05-09 16:18:36 +0300

    add config

--- server.ts
+++ server.ts
@@ -1,13 +1,19 @@
 import app from "./src/app";
+import config from "config";
+
 const startServer = async () => {
-  const PORT = process.env.PORT || 5503;
+  const PORT = config.get("server.port") || 5503;
 
   try {
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
-  } catch (err: unknown) {
-    if (err instanceof Error) {
-      console.log("Error starting server: ", err.message);
-    }
+    app
+      .listen(PORT, () => console.log(`Listening on port ${PORT}`))
+      .on("error", (err) => {
+        console.log("Error starting server: ", err.message);
+        process.exit(1);
+      });
+  } catch (err) {
+    console.log("Error happened: ", err.message);
+    process.exit(1);
   }
 };
 
