13f350b2b600a7afe17e8dc9cbca23c2e2825de3
Rakesh K <codersgyan@gmail.com>
2024-05-09 15:39:51 +0300

    setup eslint

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import app from './src/app';
-
 const startServer = async () => {
     const PORT = process.env.PORT || 5503;
+
     try {
         app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
     } catch (err: unknown) {
