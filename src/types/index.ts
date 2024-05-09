88c576c92adddaa90d96ea84463633af0e35d7ce
Rakesh K <codersgyan@gmail.com>
2024-05-09 16:42:41 +0300

    add auth request type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,14 @@
+import { Request } from "express";
+
 export type AuthCookie = {
   accessToken: string;
 };
+
+export interface AuthRequest extends Request {
+  auth: {
+    sub: string;
+    role: string;
+    id?: string;
+    tenant: string;
+  };
+}
