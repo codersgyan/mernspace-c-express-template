877a058a2365e0f98b10e4d056c56a25ea6529f5
Rakesh K <codersgyan@gmail.com>
2024-05-09 15:07:25 +0300

    create express server

--- server.ts
+++ server.ts
@@ -1,1 +1,14 @@
-console.log('server');
+import app from './src/app';
+
+const startServer = async () => {
+    const PORT = process.env.PORT || 5503;
+    try {
+        app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            console.log('Error starting server: ', err.message);
+        }
+    }
+};
+
+void startServer();
